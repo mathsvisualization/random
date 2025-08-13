91e19d52edeeaff06cb5512642e397f914eff789
mathsvisualization <sadiyavip09@gmail.com>
2025-08-13 19:45:27 +0530

    Python Basics

---
+++ python313/Basics/python_basic.py
@@ -0,0 +1,34 @@
+"""
+Python Basics Module
+
+This module covers fundamental Python concepts, focusing
+on basic data types, variables, and simple operations.
+
+Author: Shavez
+Date: 13/08/2025
+"""
+
+# Builtins data types
+
+# 1. Numeric Type
+int = 4
+float = 4.8
+complex = 1+2j
+
+# 2. Sequence Type
+str = "John"
+list = [1, 2, 3.4, "multiple obj"]
+tuple = (3+4j, "orderd", 1.0)
+
+# 3. Set Types
+set = {1, 3, 2, "unorderd", 'unique'}
+frozenset = {"unmutable", "set", 1.0}
+
+# 4. Mapping Type
+dict = {"Key": "value", "mutable": 1.0}
+
+# 5. Boolean Type
+bool = False/True
+
+# 6. None Type
+None
